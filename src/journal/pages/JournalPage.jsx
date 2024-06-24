7c155520804a0f9c86db3d9bcb7b580792b04722
raul.lopezosa <raul.lopezosa@juntaex.es>
2024-06-24 11:06:33 +0200

    inicio 08

---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,14 @@
+
+import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+
+
+export const JournalPage = () => {
+  return (
+    <JournalLayout >
+
+      <Typography > Laborum adipisicing adipisicing tempor in qui deserunt aliquip adipisicing reprehenderit magna. Consequat deserunt dolor nulla dolor non tempor ex fugiat velit. Ipsum eiusmod id mollit cillum sint ullamco elit dolore ea consectetur fugiat tempor.</Typography>
+      
+    </JournalLayout>
+  )
+}
